a9e82eacd97850687120ca26fd620650b36b97d3
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2023-02-13 20:41:22 +0530

    #1 #2

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -6,6 +6,7 @@ using System.IO;
 using UnityEngine.SceneManagement;
 using System;
 using TMPro;
+using Fove.Unity;
 
 public class MenuController : MonoBehaviour
 {
@@ -15,6 +16,7 @@ public class MenuController : MonoBehaviour
         avatar, faceFixationDuration, responseRegistrationFixationDuration, cueDeliveryDuration, experimentMetaData;
     [SerializeField] private Toggle is_enforceEyeContact, is_conditionedCueSequence, is_keepFingerPointing;
     [SerializeField] string experimentSessionDirName;
+    [SerializeField] bool enforceCalibration;
     //Temporary Hardcoded Indices
     private int[] hardCodedTargetObjects = { 0, 1, 2, 3, 4, 5, 6, 7 };
     //Main Menu button to start the experiment
@@ -22,6 +24,8 @@ public class MenuController : MonoBehaviour
     {
         try
         {
+            FoveManager.RegisterCapabilities(Fove.ClientCapabilities.EyeTracking);
+            FoveManager.RegisterCapabilities(Fove.ClientCapabilities.UserPresence);
             Debug.Log("Experiment Session started");
             //Fetching the experiment settings and convert into int
             ExperimentSettings.uid = int.Parse(uid.text);
@@ -41,14 +45,18 @@ public class MenuController : MonoBehaviour
             ExperimentSettings.targetObjectArrayIndices = string.Join("", hardCodedTargetObjects); //hardcoded value
             ExperimentSettings.experimentMetaData = experimentMetaData.text;
             ExperimentSettings.avatar = "Ashirbad";
-            //Debug.Log(ExperimentSettings.uid+ ExperimentSettings.trialCount+ ExperimentSettings.targetObjectsCount+ ExperimentSettings.faceFixationTime.ToString()+ ExperimentSettings.responseRegistrationFixationDuration+ ExperimentSettings.cueDeliverySpeed+ ExperimentSettings.enforceEyeContact+ ExperimentSettings.targetObjectArrayIndices+ ExperimentSettings.avatar+ExperimentSettings.experimentMetaData);
-            ExperimentSettings.experimentSessionStartTime = DateTime.Now.ToString();
             // Generate the experiment session Dir name and add it to Experiment Settings
             experimentSessionDirName = "JAMaster_Date_" + DateTime.Now.ToLongDateString() + "_TimeUTC_" + DateTime.Now.ToFileTimeUtc() + "_UID_" + ExperimentSettings.uid;
             ExperimentSettings.experimentSessionParentDirName = experimentSessionDirName;
-            DataDumper.DumpSessionDataExpStart();
-            //Load the next scene
-            SceneManager.LoadScene("Experiment1_Ethical_Demo");
+            // Check if user is wearing headset then only load
+            if (FoveManager.IsUserPresent())
+            {
+                LoadExperiment();
+            }
+            else
+            {
+                StartCoroutine(checkUserPresence());
+            }
         }
         catch (Exception e)
         {
@@ -56,6 +64,37 @@ public class MenuController : MonoBehaviour
             Debug.LogException(e, this);
         }
 
+    }
+    IEnumerator checkUserPresence()
+    {
+        Debug.Log("Please wear Headset");
+        yield return FoveManager.WaitForUser;
+        LoadExperiment();
+    }
+    private void EnforceCalibration()
+    {
+        Debug.Log("Enforcing Calibration Start");
+        FoveManager.StartEyeTrackingCalibration();
+        if (FoveManager.IsEyeTrackingReady())
+        {
+            //Load the next scene
+            SceneManager.LoadScene("Experiment1_Ethical_Demo");
+        }
+    }
+    void LoadExperiment()
+    {
+        // Check for Calibration Option
+        if (enforceCalibration)
+        {
+            EnforceCalibration();
+        }
+        else
+        {
+            // Register the time when the experiment session starts
+            ExperimentSettings.experimentSessionStartTime = DateTime.Now.ToString();
+            DataDumper.DumpSessionDataExpStart();//Only dump settings if we know that at least the first trial is going to start
+            SceneManager.LoadScene("Experiment1_Ethical_Demo");
+        }
     }
     // Function to dump the session data into FS and later on DB
     public void quitGame()
