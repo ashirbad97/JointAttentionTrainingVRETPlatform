efa4f8364d7b61c3a1132c1d1e0a802148b57f11
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2023-04-07 16:02:08 +0530

    Included all the Target Objects for Experiments

--- Assets/Scripts/SpawnTargetObjects.cs
+++ Assets/Scripts/SpawnTargetObjects.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnTargetObjects : MonoBehaviour
-{ 
+{
     // Loads all prefabs from the directory
     Object[] givenPrefabsList;
     Dictionary<string, GameObject> givenPrefabsDictionary = new Dictionary<string, GameObject>();//Convert the Prefabs fetched with file names as key
-    List<string> targetPrefabsList = new List<string> { "Cat",/**  "Chicken", "Flower", "Lion", "Penguin", "Tree"*/"Dog" };//Hardcoding the names of the target object prefabs that we want to spawn
+    List<string> targetPrefabsList = new List<string> { "Cat", "Chicken", "Flower", "Lion", "Penguin", "Tree", "Dog" };//Hardcoding the names of the target object prefabs that we want to spawn
     List<string> targetPrefabsToSpawn = new List<string>(); //Initializes the actual target objects which will be spawned
 
     int numberOfTargetPrefabsToSpawn; //Fetches from settings the no of target objects
@@ -24,7 +24,7 @@ public class SpawnTargetObjects : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
     //Loads Prefabs from FS and adds the pool of prefabs names from which the random taget GameObjects are to be selected
     void LoadPrefabs()
@@ -37,7 +37,7 @@ public class SpawnTargetObjects : MonoBehaviour
 
         int i = 0;
         //Feeds the list of the targetObjects choice from which we have to spawn
-        while(i < numberOfTargetPrefabsToSpawn)
+        while (i < numberOfTargetPrefabsToSpawn)
         {
             int val = Random.Range(0, targetPrefabsList.Count);
             if (!targetPrefabsToSpawn.Contains(targetPrefabsList[val]))
@@ -56,6 +56,7 @@ public class SpawnTargetObjects : MonoBehaviour
             GameObject go;
             givenPrefabsDictionary.TryGetValue(targetPrefabsToSpawn[i], out go);//Fetching value by key (name of prefab) from dictionary
             GameObject spawned = Instantiate(go, spawnLocationRefs[i].transform.position, Quaternion.identity);// Spawns the prefab into the referenced location
+            spawned.transform.Rotate(0f, 180f, 0f);
             spawned.name = go.name;//Changes the name for recording properly in Logs
             spawned.AddComponent<TargetEndPointProperty>().direction = spawnLocationRefs[i].GetComponent<TargetEndPointProperty>().direction;//Adds the property of TargetEndPoint script and assigns the direction as that of the target end point anchor direction to which it is referenced
         }
